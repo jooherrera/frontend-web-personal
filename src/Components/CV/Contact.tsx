87b5be8019f65c9b352112428bd14b6648ad6015
Jose Herrera <jooherrera4@gmail.com>
2022-04-24 01:08:52 -0300

    added linkedin

--- src/Components/CV/Contact.tsx
+++ src/Components/CV/Contact.tsx
@@ -2,6 +2,7 @@ import cellPhoneIcon from './phone.svg'
 import phoneIcon from './telephone-fill.svg'
 import mapIcon from './geo-alt-fill.svg'
 import worldIcon from './globe2.svg'
+import linkedinIcon from './linkedin.svg'
 import { useContext } from 'react'
 import { appContext } from '../../context/context'
 
@@ -19,12 +20,14 @@ export const Contact = () => {
               <img src={cellPhoneIcon} alt="cellPhone icon" />
               <img src={mapIcon} alt="map icon" />
               <img src={worldIcon} alt="world icon" />
+              <img src={linkedinIcon} alt="linkedin icon" />
             </div>
             <div className="Contact__Info">
               <h5>{state.info.resume.content.sectionContact.phone}</h5>
               <h5>{state.info.resume.content.sectionContact.mail}</h5>
               <h5>{state.info.resume.content.sectionContact.city}</h5>
               <h5>{state.info.resume.content.sectionContact.website}</h5>
+              <h5>{state.info.resume.content.sectionContact.linkedin}</h5>
             </div>
           </div>
         </div>
